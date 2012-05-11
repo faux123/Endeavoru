e0de5f8cdecffd9c7dbc78462f4fc26b27ea826e
faux123 <reioux@gmail.com>
2012-05-11 12:22:10 -0700

    ARM: idle: call idle notifiers before stopping nohz tick
    If an idle notifier modifies a timer, calling the notifier after
    the sched tick has been stopped may leave the sched tick set too
    early.  Move teh idle notifier call before the call to
    tick_nohz_stop_sched_tick.
    
    Change-Id: I0db3284bec6d0193bc5e2a57650ab06bd8342319
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -236,9 +236,9 @@ void cpu_idle(void)
 			}
 			bPrint_wake_lock = !bPrint_wake_lock;
 		}
-		tick_nohz_stop_sched_tick(1);
 		leds_event(led_idle_start);
 		idle_notifier_call_chain(IDLE_START);
+		tick_nohz_stop_sched_tick(1);
 		while (!need_resched()) {
 #ifdef CONFIG_HOTPLUG_CPU
 			if (cpu_is_offline(smp_processor_id()))
