b7ff4cc7409c3c9b22e522ae2debabb481a822a0
faux123 <reioux@gmail.com>
2012-07-27 13:44:56 -0700

    tegra video: code fix up for tegra video bandwidth underruns
    which caused screen to flicker from HTC endeavoru QMR source drop

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -988,6 +988,7 @@ static void tegra_dc_clear_bandwidth(struct tegra_dc *dc)
 static void tegra_dc_program_bandwidth(struct tegra_dc *dc)
 {
 	unsigned i;
+	bool yuvp = false;
 
 	if (dc->emc_clk_rate != dc->new_emc_clk_rate) {
 		/* going from 0 to non-zero */
@@ -995,6 +996,20 @@ static void tegra_dc_program_bandwidth(struct tegra_dc *dc)
 			clk_enable(dc->emc_clk);
 
 		dc->emc_clk_rate = dc->new_emc_clk_rate;
+
+		/*FIXME:For low resolution video the bandwidth will too low.
+			Set lower bound when emc_clk_rate too tlow.
+		*/
+		if (dc->out->video_min_bw && dc->ndev->id == 0) {
+			for (i = 0; i < DC_N_WINDOWS; i++) {
+				struct tegra_dc_win *w = &dc->windows[i];
+				yuvp |= tegra_dc_is_yuv_planar(w->fmt);
+			}
+
+			if (yuvp)
+				dc->emc_clk_rate = max(dc->out->video_min_bw,dc->emc_clk_rate);
+		}
+
 		clk_set_rate(dc->emc_clk, dc->emc_clk_rate);
 
 		if (!dc->new_emc_clk_rate) /* going from non-zero to 0 */
@@ -2062,11 +2077,6 @@ static void tegra_dc_underflow_handler(struct tegra_dc *dc)
 	int i;
 	static int underflow_cnt = 0;
 
-	val = tegra_dc_readl(dc, DC_DISP_DISP_MISC_CONTROL);
-	val ^= 0x2;
-	tegra_dc_writel(dc, val, DC_DISP_DISP_MISC_CONTROL);
-
-
 	dc->stats.underflows++;
 	if (dc->underflow_mask & WIN_A_UF_INT)
 		dc->stats.underflows_a += tegra_dc_underflow_count(dc,
@@ -2079,9 +2089,9 @@ static void tegra_dc_underflow_handler(struct tegra_dc *dc)
 			DC_WINBUF_CD_UFLOW_STATUS);
 
 	if (underflow_cnt < UNDERFLOW_MAXLOG) {
-		printk(KERN_ERR "[DISP] dc underflow: %llu a: %llu b: %llu c: %llu emc_rate %d UF_LINE_FLUSH 0x%8x\n",
+		printk(KERN_ERR "[DISP] dc underflow: %llu a: %llu b: %llu c: %llu emc_rate %d\n",
 			dc->stats.underflows,dc->stats.underflows_a, dc->stats.underflows_b
-			, dc->stats.underflows_c, dc->emc_clk_rate,val);
+			, dc->stats.underflows_c, dc->emc_clk_rate);
 		underflow_cnt++;
 	}
 
@@ -2095,6 +2105,13 @@ static void tegra_dc_underflow_handler(struct tegra_dc *dc)
 				schedule_work(&dc->reset_work);
 				/* reset counter */
 				dc->windows[i].underflows = 0;
+			}
+#endif
+#ifdef CONFIG_ARCH_TEGRA_3x_SOC
+			if (dc->windows[i].underflows > 4) {
+				val = tegra_dc_readl(dc, DC_DISP_DISP_MISC_CONTROL);
+				val |= UF_LINE_FLUSH;
+				tegra_dc_writel(dc, val, DC_DISP_DISP_MISC_CONTROL);
 			}
 #endif
 		} else {
@@ -2247,6 +2264,8 @@ static irqreturn_t tegra_dc_irq(int irq, void *ptr)
 		schedule_delayed_work(&dc->underflow_work,
 			msecs_to_jiffies(1));
 	}
+	else
+		tegra_dc_writel(dc, 0, DC_DISP_DISP_MISC_CONTROL);
 
 	if (dc->out->flags & TEGRA_DC_OUT_ONE_SHOT_MODE)
 		tegra_dc_one_shot_irq(dc, status);
@@ -2401,7 +2420,9 @@ static void tegra_dc_init(struct tegra_dc *dc)
 	tegra_dc_writel(dc, 0x0001c700, DC_CMD_INT_POLARITY);
 	tegra_dc_writel(dc, 0x00202020, DC_DISP_MEM_HIGH_PRIORITY);
 	tegra_dc_writel(dc, 0x00010101, DC_DISP_MEM_HIGH_PRIORITY_TIMER);
-
+#ifdef CONFIG_ARCH_TEGRA_3x_SOC
+	tegra_dc_writel(dc, 0x00000000, DC_DISP_DISP_MISC_CONTROL);
+#endif
 	/* enable interrupts for vblank, frame_end and underflows */
 	tegra_dc_writel(dc, (FRAME_END_INT | V_BLANK_INT | ALL_UF_INT),
 		DC_CMD_INT_ENABLE);
